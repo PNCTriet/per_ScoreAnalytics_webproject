18717d17841359eb3dc6036dfd1fc264d5795a8d
Triết Nguyễn Phạm <triet.pnc@gmail.com>
2024-07-17 16:03:46 +0700

    learn how to uses route

---
+++ src/app/user/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { useRouter } from "next/navigation"
+
+const VotenowUser = () => {
+    const router = useRouter()
+
+    const handleBtn = () => {
+        router.push("/")
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-black-100 p-4">
+            <h1 className="text-3xl font-bold text-blue-500 mb-4">Votenow User</h1>
+            <button 
+                onClick={handleBtn} 
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Back to Home
+            </button>
+        </div>
+    )
+}
+
+export default VotenowUser;
